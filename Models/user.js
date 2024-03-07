c42dee1461ad965ac2cb192e740af4df396ec559
jlinl16 <junlinl4@nycstudents.net>
2024-03-07 16:05:37 -0500

    dsfdslk

--- Models/user.js
+++ Models/user.js
@@ -6,6 +6,7 @@ const userSchema = new mongoose.Schema({
         type: String,
         trim:true, 
         required: "Please provide a username",
+        unique: true
     },
     password: {
         type: String,
@@ -16,16 +17,44 @@ const userSchema = new mongoose.Schema({
         type: Array,
         trim:true,
     },
+    tokens: [
+        {
+          token: {
+            type: String,
+            required: true,
+          },
+        },
+      ],
+    
 
 });
 
-userSchema.pre('save', function (next) {
-    if(!this.isModified("name")) {
-        next();
-        return;
+// userSchema.pre('save', function (next) {
+//     if(!this.isModified("name")) {
+//         next();
+//         return;
+//     }
+//     this.slug=slugify(this.name);
+//     next();
+// });
+
+// module.exports = mongoose.model("User", userSchema); 
+
+userSchema.pre("save", async function (next) {
+    const user = this;
+    if (user.isModified("password")) {
+      user.password = await bcrypt.hash(user.password, 8);
     }
-    this.slug=slugify(this.name);
+  
     next();
-});
-
-module.exports = mongoose.model("User", userSchema); 
+  });
+  userSchema.methods.comparePassword = function (passw, cb) {
+    bcrypt.compare(passw, this.password, function (err, isMatch) {
+      if (err) {
+        return cb(err);
+      }
+      cb(null, isMatch);
+    });
+  };
+  
+  module.exports = mongoose.model("User", userSchema);
