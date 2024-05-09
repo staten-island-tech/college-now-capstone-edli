72bd2f474a57b3679021359138e536a6443f09a2
jlinl16 <158515720+jlinl16@users.noreply.github.com>
2024-05-09 14:35:37 -0400

    fjaskf

--- front-end/src/components/SignUp.vue
+++ front-end/src/components/SignUp.vue
@@ -1,19 +1,51 @@
 <template>
-    <form name="login-form" @submit.prevent="login(username, password)">
-        <div>
-          <label for="username">Username: </label>
-          <input id="username" type="text" v-model="username" />
-        </div>
-        <div>
-          <label for="password">Password: </label>
-          <input id="password" type="password" v-model="password" />
-        </div>
-        <button class="submit-button" type="submit">Sign Up</button>
-      </form>
+    <div class ="signupcard">
+        <form name="login-form" @submit.prevent="login(username, password)">
+            <div>
+              <label for="username">Username: </label>
+              <input id="username" type="text" v-model="username" />
+            </div>
+            <div>
+              <label for="password">Password: </label>
+              <input id="password" type="password" v-model="password" />
+            </div>
+            <button class="submit-button" type="submit">Sign Up</button>
+          </form>
+        <p v-if="signupSuccess">{{ "Login Success!" }}</p>
+    </div>
     </template>
     
-    <script setup>
-    
+<script setup>
+import { ref } from 'vue'
+
+const username = ref("");
+const password = ref("");
+const signupSuccess = ref(false);
+
+async function register() { 
+  try {
+    const res = await fetch("http://localhost:9999/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: username.value.toLowerCase(),
+        password: password.value,
+      }),
+    });
+    const user = await res.json();
+    console.log(user);
+    if (res.ok) {
+      localStorage.setItem("token", user.token);
+      signupSuccess.value = true; 
+    } else {
+      console.log(error);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
     </script>
     
     <style scoped>
