1ef1b717c6352f7f6e565f17d3ce540ffdc4f1c9
jlinl16 <158515720+jlinl16@users.noreply.github.com>
2024-05-09 14:37:35 -0400

    sflskdkfl

--- front-end/src/views/SignUp.vue
+++ front-end/src/views/SignUp.vue
@@ -1,42 +1,12 @@
 <template>
     <div>
-      <div>
         <SignUp v-if="user" />
-      </div>
     </div>
   </template>
 
 <script setup>
-import { ref } from "vue";
 import SignUp from "../components/SignUp.vue";
-const username = ref("");
-const password = ref("");
-const user = true;
-function notSignup(username, password) {
-  console.log(username);
-  console.log(password);
-}
-function change() {
-  user = false;
-}
-async function signup(username, password) {
-  try {
-    const res = await fetch("http://localhost:5173/signup", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        username: username.toLowerCase(),
-        password: password,
-      }),
-    });
-    const user = await res.json();
-    console.log(user);
-  } catch (error) {
-    console.log(error);
-  }
-}
+
 </script>
 
 <style scoped></style>
