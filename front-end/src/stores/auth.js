236efb0f77419de399671e3633e478b2f285a672
jlinl16 <158515720+jlinl16@users.noreply.github.com>
2024-05-09 14:24:38 -0400

    dsilfaidsf

--- front-end/src/stores/auth.js
+++ front-end/src/stores/auth.js
@@ -1,18 +1,34 @@
 
-import { defineStore } from "pinia";
+import { defineStore } from 'pinia'
 
-export const authStore = defineStore("user", {
-  state: () => {
-    return {
-        user: {
-            id: ,
-            name:,
-      },
-    };
-  },
-  actions: [
-    async login({username, password}) {
-        
+export const authStore = defineStore({
+  id: 'auth',
+  state: () => ({
+    authenticated: false
+  }),
+  actions: {
+    async login({ username, password }) {
+      try {
+        const res = await fetch('http://localhost:9999/login', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            username: username.toLowerCase(),
+            password: password
+          })
+        })
+        const user = await res.json()
+        console.log(user)
+        if (res.ok) {
+          this.authenticated = true
+        } else {
+          console.log("not authed")
+        }
+      } catch (error) {
+        console.log(error)
+      }
     }
-  ]
-});
+  }
+})
